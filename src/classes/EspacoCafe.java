f1af29aa6073613428f5c7f187ba8654b5db3e7d
VitorNathanG <78208667+VitorNathanG@users.noreply.github.com>
2021-02-22 19:05:41 -0300

    Layouts das tabelas parcialmente completos

--- src/classes/EspacoCafe.java
+++ src/classes/EspacoCafe.java
@@ -2,12 +2,20 @@ package classes;
 
 public class EspacoCafe extends Espaco {
 
-
+    private static int lotacao;
     public EspacoCafe(String nomeEspaco) {
         super(nomeEspaco);
     }
 
     public EspacoCafe() {
         super();
+    }
+
+    public static int getLotacao() {
+        return lotacao;
+    }
+
+    public static void setLotacao(int lotacao) {
+        EspacoCafe.lotacao = lotacao;
     }
 }
--- src/layout/PrincipalController.java
+++ src/layout/PrincipalController.java
@@ -17,6 +17,7 @@ public class PrincipalController implements Initializable {
 
     @FXML private AnchorPane janela;
 
+    @FXML private MenuBar barraDeMenu;
     @FXML private MenuItem abrirArquivoSalas;
     @FXML private MenuItem abrirArquivoParticipantes;
     @FXML private MenuItem abrirArquivoEspacosCafe;
@@ -29,15 +30,30 @@ public class PrincipalController implements Initializable {
     @FXML private Button mudarParaParticipantes;
     @FXML private Button mudarParaEspacosCafe;
 
+    @FXML private AnchorPane anchorPaneTabelas;
+
     @FXML private TableView tabelaParticipantes;
     @FXML private TableColumn colunaNome;
     @FXML private TableColumn colunaSobrenome;
 
-    @FXML private TextField nomeParticipanteAdicionar;
-    @FXML private Button nomeParticipanteAdicionarBotao;
+    @FXML private TableView tabelaSalas;
+    @FXML private TableColumn colunaIdentificacaoSala;
 
-    @FXML private Button botaoAbrirDetalhes;
-    @FXML private Button botaoExcluir;
+    @FXML private AnchorPane adicionarParticipantePainel;
+    @FXML private TextField adicionarParticipanteNome;
+    @FXML private Button adicionarParticipanteBotao;
+    @FXML private Label adicionarParticipanteLabel;
+
+    @FXML private AnchorPane adicionarSalaPainel;
+    @FXML private TextField adicionarSalaNome;
+    @FXML private Button adicionarSalaBotao;
+    @FXML private Label adicionarSalaLabel;
+
+    @FXML private Button botaoAbrirDetalhesParticipante;
+    @FXML private Button botaoExcluirParticipante;
+
+    @FXML private Button botaoAbrirDetalhesSala;
+    @FXML private Button botaoExcluirSala;
 
     File infoEspacosCafe = FileHandler.criarArquivo("dados\\InfoEspacosCafe.data");
     ObservableList<EspacoCafe> espacosCafe = DataHandler.gerarEspacosCafe(DataHandler.recuperarDadosSalvos(infoEspacosCafe));
@@ -52,49 +68,125 @@ public class PrincipalController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources){
 
+
+
+        tabelaParticipantes.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if(newVal != null) {
+                botaoExcluirParticipante.setDisable(false);
+                botaoAbrirDetalhesParticipante.setDisable(false);
+            } else {
+                botaoExcluirParticipante.setDisable(true);
+                botaoAbrirDetalhesParticipante.setDisable(true);
+            }
+        });
+        janelasReativas();
+        definirTabelas();
+        visibilidadeInicial();
+    }
+
+    public void adicionarParticipanteBotaoClicked() {
+        pessoas.add(new Pessoa(Pessoa.separarNome(adicionarParticipanteNome.getText()),
+                Pessoa.separarSobrenome(adicionarParticipanteNome.getText()), "", "",
+                "", ""));
+        adicionarParticipanteNome.setText("");
+        DataHandler.salvarDados(infoPessoas, pessoas, new Pessoa());
+    }
+
+    public void adicionarSalaBotaoClicked() {
+        salasTreinamento.add(new EspacoTreinamento(adicionarSalaNome.getText(), EspacoTreinamento.getLotacao()));
+        adicionarSalaNome.setText("");
+        DataHandler.salvarDados(infoSalasTreinamento, salasTreinamento, new EspacoTreinamento());
+    }
+
+    public void mudarParaParticipantesClicked() {
+        adicionarParticipantePainel.setVisible(true);
+        tabelaParticipantes.setVisible(true);
+        botaoExcluirParticipante.setVisible(true);
+        botaoAbrirDetalhesParticipante.setVisible(true);
+
+        tabelaSalas.setVisible(false);
+        adicionarSalaPainel.setVisible(false);
+        botaoAbrirDetalhesSala.setVisible(false);
+        botaoExcluirSala.setVisible(false);
+    }
+
+    public void mudarParaSalasClicked() {
+        tabelaParticipantes.setVisible(false);
+        adicionarParticipantePainel.setVisible(false);
+        botaoExcluirParticipante.setVisible(false);
+        botaoAbrirDetalhesParticipante.setVisible(false);
+
+        tabelaSalas.setVisible(true);
+        adicionarSalaPainel.setVisible(true);
+        botaoAbrirDetalhesSala.setVisible(true);
+        botaoExcluirSala.setVisible(true);
+    }
+
+    public void mudarParaEspacosCafeClicked() {
+        tabelaParticipantes.setVisible(false);
+        adicionarParticipantePainel.setVisible(false);
+        botaoExcluirParticipante.setVisible(false);
+        botaoAbrirDetalhesParticipante.setVisible(false);
+
+        tabelaSalas.setVisible(false);
+        adicionarSalaPainel.setVisible(false);
+        botaoAbrirDetalhesSala.setVisible(false);
+        botaoExcluirSala.setVisible(false);
+    }
+
+    public void visibilidadeInicial(){
+        adicionarParticipantePainel.setVisible(true);
+        tabelaParticipantes.setVisible(true);
+        botaoExcluirParticipante.setVisible(true);
+        botaoAbrirDetalhesParticipante.setVisible(true);
+
+        adicionarSalaPainel.setVisible(false);
+        tabelaSalas.setVisible(false);
+        botaoExcluirSala.setVisible(false);
+        botaoAbrirDetalhesSala.setVisible(false);
+    }
+
+    public void janelasReativas(){
         janela.widthProperty().addListener((obs, oldVal, newVal) -> {
             tabelaParticipantes.setMaxWidth((Math.round((double) newVal)) - 180);
             tabelaParticipantes.setMinWidth((Math.round((double) newVal)) - 180);
+
+            tabelaSalas.setMaxWidth((Math.round((double) newVal)) - 180);
+            tabelaSalas.setMinWidth((Math.round((double) newVal)) - 180);
+
+            tabelaParticipantes.setMaxWidth((Math.round((double) newVal)) - 180);
+            tabelaParticipantes.setMinWidth((Math.round((double) newVal)) - 180);
         });
+
         tabelaParticipantes.widthProperty().addListener((obs, oldVal, newVal) -> {
             colunaSobrenome.setMaxWidth((Math.round((double) newVal)) - 200);
             colunaSobrenome.setMinWidth((Math.round((double) newVal)) - 200);
         });
 
-        tabelaParticipantes.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
-            if(newVal != null) {
-                botaoExcluir.setDisable(false);
-                botaoAbrirDetalhes.setDisable(false);
-            } else {
-                botaoExcluir.setDisable(true);
-                botaoAbrirDetalhes.setDisable(true);
-            }
+        tabelaSalas.widthProperty().addListener((obs, oldVal, newVal) -> {
+            colunaIdentificacaoSala.setMaxWidth((Math.round((double) newVal)));
+            colunaIdentificacaoSala.setMinWidth((Math.round((double) newVal)));
         });
 
+        janela.heightProperty().addListener((obs, oldVal, newVal) -> {
+            anchorPaneTabelas.setMaxHeight((Math.round((double) newVal)) - 80);
+            anchorPaneTabelas.setMinHeight((Math.round((double) newVal)) - 80);
+        });
+    }
+
+    public void definirTabelas(){
         colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
         colunaSobrenome.setCellValueFactory(new PropertyValueFactory<>("sobrenome"));
 
         colunaNome.setCellFactory(TextFieldTableCell.forTableColumn());
         colunaSobrenome.setCellFactory(TextFieldTableCell.forTableColumn());
 
-
         tabelaParticipantes.setItems(pessoas);
 
-        DataHandler.salvarDados(infoPessoas, pessoas, new Pessoa());
-        DataHandler.salvarDados(infoEspacosCafe, espacosCafe, new EspacoCafe());
-        DataHandler.salvarDados(infoSalasTreinamento, salasTreinamento, new EspacoTreinamento());
-        System.out.println("Arquivos Salvos");
+        colunaIdentificacaoSala.setCellValueFactory(new PropertyValueFactory<>("nomeEspaco"));
+        colunaIdentificacaoSala.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        tabelaSalas.setItems(salasTreinamento);
     }
 
-    public void nomeParticipanteAdicionarBotaoClicked() {
-        pessoas.add(new Pessoa(Pessoa.separarNome(nomeParticipanteAdicionar.getText()),
-                Pessoa.separarSobrenome(nomeParticipanteAdicionar.getText()), "", "",
-                "", ""));
-        nomeParticipanteAdicionar.setText("");
-        DataHandler.salvarDados(infoPessoas, pessoas, new Pessoa());
-    }
-
-    public void mudarParaParticipantesClicked() {
-        tabelaParticipantes.setVisible(true);
-    }
 }
