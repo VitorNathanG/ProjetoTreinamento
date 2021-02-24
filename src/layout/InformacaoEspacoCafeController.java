ddc9c3ef432101dbe5fc8070146bdddc69a56112
VitorNathanG <78208667+VitorNathanG@users.noreply.github.com>
2021-02-24 10:49:48 -0300

    Detalhes dos espaços completos

--- src/layout/InformacaoEspacoCafeController.java
+++ src/layout/InformacaoEspacoCafeController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class InformacaoEspacoCafeController implements Initializable {
@@ -18,8 +19,8 @@ public class InformacaoEspacoCafeController implements Initializable {
 
     @FXML private ListView listaParticipantesPrimeiraEtapa;
     @FXML private ListView listaParticipantesSegundaEtapa;
-    @FXML private ListView participantesQueSaem;
-    @FXML private ListView participantesQueVem;
+    @FXML private ListView listaParticipantesQueSaem;
+    @FXML private ListView listaParticipantesQueVem;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -28,6 +29,38 @@ public class InformacaoEspacoCafeController implements Initializable {
         lotacaoEspacoCafe.setText(String.valueOf(espacoCafe.getLotacao()));
         totalIntegrantesPrimeiraEtapa.setText(String.valueOf(espacoCafe.getIntegrantesPrimeiraEtapa().size()));
         totalIntegrantesSegundaEtapa.setText(String.valueOf(espacoCafe.getIntegrantesSegundaEtapa().size()));
+
+        ArrayList<String> nomesParticipantesPrimeiraEtapa = new ArrayList<>();
+        ArrayList<String> nomesParticipantesSegundaEtapa = new ArrayList<>();
+        ArrayList<String> nomesParticipantesQueSaem = new ArrayList<>();
+        ArrayList<String> nomesParticipantesQueVem = new ArrayList<>();
+
+        for (int i = 0; i < espacoCafe.getIntegrantesPrimeiraEtapa().size(); i++) {
+            nomesParticipantesPrimeiraEtapa.add(espacoCafe.getIntegrantesPrimeiraEtapa().get(i).getNome() + " " +
+                    espacoCafe.getIntegrantesPrimeiraEtapa().get(i).getSobrenome());
+        }
+        for (int i = 0; i < espacoCafe.getIntegrantesSegundaEtapa().size(); i++) {
+            nomesParticipantesSegundaEtapa.add(espacoCafe.getIntegrantesSegundaEtapa().get(i).getNome() + " " +
+                    espacoCafe.getIntegrantesSegundaEtapa().get(i).getSobrenome());
+        }
+
+        for (String participante: nomesParticipantesPrimeiraEtapa) {
+            if(!nomesParticipantesSegundaEtapa.contains(participante)){
+                nomesParticipantesQueSaem.add(participante);
+            }
+        }
+
+        for (String participante: nomesParticipantesSegundaEtapa) {
+            if(!nomesParticipantesPrimeiraEtapa.contains(participante)){
+                nomesParticipantesQueVem.add(participante);
+            }
+        }
+
+        listaParticipantesPrimeiraEtapa.getItems().addAll(nomesParticipantesPrimeiraEtapa);
+        listaParticipantesSegundaEtapa.getItems().addAll(nomesParticipantesSegundaEtapa);
+        listaParticipantesQueSaem.getItems().addAll(nomesParticipantesQueSaem);
+        listaParticipantesQueVem.getItems().addAll(nomesParticipantesQueVem);
+
 
     }
 }
--- src/layout/InformacaoSalaController.java
+++ src/layout/InformacaoSalaController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class InformacaoSalaController implements Initializable {
@@ -18,8 +19,8 @@ public class InformacaoSalaController implements Initializable {
 
     @FXML private ListView listaParticipantesPrimeiraEtapa;
     @FXML private ListView listaParticipantesSegundaEtapa;
-    @FXML private ListView participantesQueSaem;
-    @FXML private ListView participantesQueVem;
+    @FXML private ListView listaParticipantesQueSaem;
+    @FXML private ListView listaParticipantesQueVem;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -28,6 +29,36 @@ public class InformacaoSalaController implements Initializable {
         lotacaoSalaTreinamento.setText(String.valueOf(salaTreinamento.getLotacao()));
         totalIntegrantesPrimeiraEtapa.setText(String.valueOf(salaTreinamento.getIntegrantesPrimeiraEtapa().size()));
         totalIntegrantesSegundaEtapa.setText(String.valueOf(salaTreinamento.getIntegrantesSegundaEtapa().size()));
+        ArrayList<String> nomesParticipantesPrimeiraEtapa = new ArrayList<>();
+        ArrayList<String> nomesParticipantesSegundaEtapa = new ArrayList<>();
+        ArrayList<String> nomesParticipantesQueSaem = new ArrayList<>();
+        ArrayList<String> nomesParticipantesQueVem = new ArrayList<>();
+
+        for (int i = 0; i < salaTreinamento.getIntegrantesPrimeiraEtapa().size(); i++) {
+            nomesParticipantesPrimeiraEtapa.add(salaTreinamento.getIntegrantesPrimeiraEtapa().get(i).getNome() + " " +
+                                                  salaTreinamento.getIntegrantesPrimeiraEtapa().get(i).getSobrenome());
+        }
+        for (int i = 0; i < salaTreinamento.getIntegrantesSegundaEtapa().size(); i++) {
+            nomesParticipantesSegundaEtapa.add(salaTreinamento.getIntegrantesSegundaEtapa().get(i).getNome() + " " +
+                    salaTreinamento.getIntegrantesSegundaEtapa().get(i).getSobrenome());
+        }
+
+        for (String participante: nomesParticipantesPrimeiraEtapa) {
+            if(!nomesParticipantesSegundaEtapa.contains(participante)){
+                nomesParticipantesQueSaem.add(participante);
+            }
+        }
+
+        for (String participante: nomesParticipantesSegundaEtapa) {
+            if(!nomesParticipantesPrimeiraEtapa.contains(participante)){
+                nomesParticipantesQueVem.add(participante);
+            }
+        }
+
+        listaParticipantesPrimeiraEtapa.getItems().addAll(nomesParticipantesPrimeiraEtapa);
+        listaParticipantesSegundaEtapa.getItems().addAll(nomesParticipantesSegundaEtapa);
+        listaParticipantesQueSaem.getItems().addAll(nomesParticipantesQueSaem);
+        listaParticipantesQueVem.getItems().addAll(nomesParticipantesQueVem);
 
     }
 }
